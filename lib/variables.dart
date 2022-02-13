4c515196729f3b0e132e2ac8e8b3759fcb719f16
Somto Chike-Nwaka <somteenwaka@gmail.com>
2022-02-13 14:12:27 +0000

    added comments for readability

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
+//this widget styles all the titles for all pages
 TextStyle titleStyle = const TextStyle(
   fontSize: 50,
   fontFamily: "Kanit",
   fontWeight: FontWeight.bold,
 );
-
+//this widget styles all the descriptions for all pages
 TextStyle descStyle = const TextStyle(
   fontSize: 20,
   fontFamily: "Montserrat",
 );
+//all titles have been refactored into a list here
 Text firstTitle = Text("Welcome!", style: titleStyle);
 Text secondTitle = Text("Customize", style: titleStyle);
 Text thirdTitle = Text("Let's go...", style: titleStyle);
 
 List<Text> titles = [firstTitle, secondTitle, thirdTitle];
 
+//These widgets are for the body texts for all pages
 Text firstDescription = Text(
     "Explore the all new way to build your fitness & find the perfect fit for you.",
     style: descStyle);
@@ -27,7 +30,7 @@ Text secondDescription = Text(
 Text thirdDescription = Text(
     "GEt the new fitness updates everyday & improve performance.",
     style: descStyle);
-
+//all descriptions have been refactored into a list here
 List<Text> descriptions = [
   firstDescription,
   secondDescription,
