078de81571d9a8fa795f2465b3e13c12adcfced7
Amoro <amorobg@gmail.com>
2022-02-13 14:10:11 +0000

    Setup the Third Screen

---
+++ lib/third_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Third Screen"),
+        ),
+        body: Center(
+          child: const Text(
+            'This is the Third Screen\'s skeleton',
+          ),
+        ),
+      ),
+    );
+  }
+}
