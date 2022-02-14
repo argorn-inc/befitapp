5a6dc39bcc46fa2a7922508506295170c05480ec
argorn-inc <info@argorninc@gmail.com>
2022-02-14 10:42:29 +0000

    modified the lowercontainer() widget to take a image name as a string

--- lib/models.dart
+++ lib/models.dart
@@ -1,60 +1,68 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:befitapp/buttons.dart';
 
-Widget upper_container(String image, Color color,var width ){
+Widget upper_container(String bgImageSrc, Color color, var width) {
+  Image bgImage = Image.asset(
+    "images/$bgImageSrc",
+    height: 300,
+    width: 600,
+  );
   return Positioned(
-
     child: Row(
       mainAxisAlignment: MainAxisAlignment.center,
-
       children: [
-
         Container(
-          width:width,
-          height: 400,
+          width: width,
+          height: 600,
           color: color,
-          child: Image.asset('images/gympics.png', width: 300,height: 600,),
+          child: bgImage,
         ),
-
       ],
     ),
   );
 }
 
-
-Widget lower_container(var width, var height, TextStyle titleStyle, TextStyle descStyle){
+Widget lower_container(
+    var width, var height, TextStyle titleStyle, TextStyle descStyle) {
   return Positioned(
       top: 300,
       child: Container(
-
         width: width,
         height: height,
-
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30))
-        ),
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30))),
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 30,),
-            Text('Welcome!', style: titleStyle,),
-            SizedBox(height: 20,),
-            Container(
-              margin:EdgeInsets.fromLTRB(10, 0, 10, 0) ,
-              child: Text('Explore the all new way to build your fitness & find the perfect fit for you', style: descStyle,textAlign: TextAlign.center,)
-              ,
+            const SizedBox(
+              height: 30,
             ),
-
-            Padding(padding: EdgeInsets.only(left: 10, right: 10),
+            Text(
+              'Welcome!',
+              style: titleStyle,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Text(
+                'Explore the all new way to build your fitness & find the perfect fit for you',
+                style: descStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            // SizedBox(
+            //   height: 80,
+            // ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
               child: Row(
-
-                children: [
-                  skipBtn(),
-                  Expanded(child: Container()),
-                  nextBtn()
-                ],
+                children: [skipBtn(), Expanded(child: Container()), nextBtn()],
               ),
             )
           ],
